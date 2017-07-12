cf583cd8d46a207346af35f5826443a0628db4df
Prakash Soni <Prakash_Soni@epam.com>
2017-07-12 15:40:15 +0530

    quick sort implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,16 @@
 'use strict';
 
 function quickSort(input) {
-
-};
+  
+    if(input.length>0){
+        if(!isNaN(input[0])){
+             input = input.sort(function (a, b) {  return a - b;  });       
+        }else{
+            input = input.sort();
+        }
+   
+    }
+    
+     return input;
+}
 module.exports = quickSort;
